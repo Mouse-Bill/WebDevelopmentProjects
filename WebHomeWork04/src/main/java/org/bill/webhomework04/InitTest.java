4125763fd9a8e76924af7f601acb6bde6eb150b3
Bill <luohaonian@foxmail.com>
2023-04-28 22:56:19 +0800

    homework04

---
+++ WebHomeWork04/src/main/java/org/bill/webhomework04/InitTest.java
@@ -0,0 +1,26 @@
+package org.bill.webhomework04;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class InitTest extends HelloServlet{
+    private String message;
+
+    public void init(){
+        message = getServletConfig().getInitParameter("msg01");
+        System.out.println("Servlet init() called with msg01 = " + message);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+}
