24a4b658359bd6ecfe3249cfa2f808da3b666046
Bill <luohaonian@foxmail.com>
2023-05-09 17:18:19 +0800

    half done

---
+++ WebHomeWork09/src/main/java/org/bill/webhomework09/CountTest.java
@@ -0,0 +1,20 @@
+package org.bill.webhomework09;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CountTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession();
+        ServletContext app = req.getServletContext();
+        resp.setContentType("text/html");
+        resp.getWriter().println("<html><body style = \"font-family: sans-serif\">");
+        resp.getWriter().println("<h1>Count: " + app.getAttribute("count") + "</h1>");
+    }
+}
---
+++ WebHomeWork09/src/main/java/org/bill/webhomework09/listener/AppListener.java
@@ -0,0 +1,35 @@
+package org.bill.webhomework09.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+import java.io.*;
+
+public class AppListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        InputStream input = this.getClass().getClassLoader().getResourceAsStream("count.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(input));
+        try{
+            String count = br.readLine();
+            br.close();
+            sce.getServletContext().setAttribute("count", Integer.parseInt(count));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        int count = (int) sce.getServletContext().getAttribute("count");
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(sce.getServletContext().getRealPath("/WEB-INF/classes/count.txt")));
+            bw.write(String.valueOf(count));
+            bw.newLine();
+            bw.flush();
+            bw.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
