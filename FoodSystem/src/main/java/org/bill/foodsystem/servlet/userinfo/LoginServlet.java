eb9f06470b6f275e5ecccfacd25e895201ad24ac
Bill <luohaonian@gmail.com>
2023-05-15 01:28:15 +0800

    05150200

--- FoodSystem/src/main/java/org/bill/foodsystem/servlet/userinfo/LoginServlet.java
+++ FoodSystem/src/main/java/org/bill/foodsystem/servlet/userinfo/LoginServlet.java
@@ -1,6 +1,7 @@
 package org.bill.foodsystem.servlet.userinfo;
 
 import com.google.gson.Gson;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,11 +11,15 @@ import jakarta.servlet.http.HttpSession;
 import org.bill.foodsystem.entity.Userinfo;
 import org.bill.foodsystem.service.UserinfoService;
 import org.bill.foodsystem.service.impl.UserinfoServiceImpl;
+import org.bill.foodsystem.util.jwt.JWT;
 
 import javax.swing.*;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.bill.foodsystem.util.jwt.JWT.createJWT;
+import static org.bill.foodsystem.util.jwt.JWT.parseJWT;
 
 @WebServlet("/userinfo/login")
 public class LoginServlet extends HttpServlet {
@@ -25,30 +30,57 @@ public class LoginServlet extends HttpServlet {
 
         response.setContentType("application/json;charset=utf-8");
 
+        String token = request.getHeader("token");
 
-        String reqBody = request.getReader().readLine();
-        if (reqBody != null) {
-            userinfo = new Gson().fromJson(reqBody, Userinfo.class);
+        if (token != null) {
+            System.out.println(token);
+            try {
+                Claims claims =  JWT.parseJWT(token);
+                if (claims != null) {
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    map.put("isOK", true);
+                    response.getWriter().write(new Gson().toJson(map));
+                }else {
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    map.put("isOK", false);
+                    response.getWriter().write(new Gson().toJson(map));
+                }
+                return;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }else {
-            String username = request.getParameter("username");
-            String passwd = request.getParameter("passwd");
-            userinfo = new Userinfo(null, username, passwd, null);
+            String reqBody = request.getReader().readLine();
+            if (reqBody != null) {
+                userinfo = new Gson().fromJson(reqBody, Userinfo.class);
+            } else {
+                String username = request.getParameter("username");
+                String passwd = request.getParameter("passwd");
+                userinfo = new Userinfo(null, username, passwd, null);
+            }
+
+            Userinfo user = userinfoService.login(userinfo);
+
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("isOK", false);
+
+            if (user != null) {
+                try {
+                    token = JWT.createJWT(String.valueOf(user.getUserid()), user.getUsername(), 1000 * 60 * 60);
+                    response.addHeader("token", token);
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                map.put("isOK", true);
+                HttpSession session = request.getSession();
+                session.setAttribute("loginUser", user);
+                response.addHeader("token", session.getId());
+            }
+
+            response.getWriter().write(new Gson().toJson(map));
         }
-
-        Userinfo user = userinfoService.login(userinfo);
-
-        Map<String, Object> map = new HashMap<String, Object>();
-
-        map.put("isOK", false);
-
-        if (user != null) {
-            map.put("isOK", true);
-            HttpSession session = request.getSession();
-            session.setAttribute("loginUser", user);
-            response.addHeader("token", session.getId());
-        }
-
-        response.getWriter().write(new Gson().toJson(map));
     }
 
     @Override
---
+++ FoodSystem/src/main/java/org/bill/foodsystem/util/jwt/JWT.java
@@ -0,0 +1,54 @@
+package org.bill.foodsystem.util.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+
+public class JWT {
+
+    private static final String profiles="webapp";
+    private static SecretKey generalKey(){
+        String stringKey = profiles;
+        byte[] encodedKey = Base64.decodeBase64(stringKey);
+        SecretKey key = new SecretKeySpec(encodedKey, 0, 256, "AES");
+        return key;
+    }
+
+    public static String createJWT(String id, String subject, long ttlMillis) throws Exception {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        SecretKey key = generalKey();
+        JwtBuilder builder = Jwts.builder()
+                .setId(id)
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .signWith(signatureAlgorithm, key);
+
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+        return builder.compact();
+    }
+
+    public static Claims parseJWT(String jwt) throws Exception{
+        SecretKey key = generalKey();
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(jwt).getBody();
+        Date exp = claims.getExpiration();
+        if (exp.before(new Date())) {
+            return null;
+        }
+
+        return claims;
+    }
+}
